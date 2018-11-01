6dcba044080e441ffe34480b1dc0df292b12efeb
BehindSoftware <berkaysaydam@iyte.edu.tr>
2018-11-01 20:00:15 +0300

    n-1

--- queue.c
+++ queue.c
@@ -1,151 +1,94 @@
 #include <stdio.h>
 #include "queue.h"
 
+TskNodePtr headPtr = NULL;
+TskNodePtr tailPtr = NULL;
 
-int Task_dequeue( Tsk_node_s *headPtr, Tsk_node_s *tailPtr, Tsk_node_s *node )
+int Task_dequeue( TskNodePtr *headPtr, TskNodePtr *tailPtr, Tsk_node_s *node )
 {
 	int value;
-	tsk_node_s tempPtr;
+	TskNodePtr tempPtr;
 
-	value = ( *headPtr ) -> data;
+	node = *headPtr;
+//	node->task_num = (*headPtr)->task_num;
+//	node->task_type= (*headPtr)->task_type;
+//	node->value = (*headPtr)->value;
+
+	value = ( *headPtr ) -> value;
 	tempPtr = *headPtr;
-	*headPtr = ( *headPtr ) -> nextPtr;
+	*headPtr = ( *headPtr ) -> next;
 
-	if ( *headPtr == NULL ) {
+	if (*headPtr == NULL){
 		*tailPtr = NULL;
 	}
 
-	free( tempPtr );
+
+	free(tempPtr);
 	return value;
 }
 
-void Task_enqueue(Tsk_node_s *headPtr, Tsk_node_s *tailPtr, Tsk_node_s *node)
+void Task_enqueue(TskNodePtr *headPtr, TskNodePtr *tailPtr, Tsk_node_s *node)
 {
-	Tsk_node_s newPtr;
+	TskNodePtr newPtr;
 
 	newPtr = malloc(sizeof(Tsk_node_s));		//create new ptr for task
 
 	if(newPtr != NULL){				//if malloc is successful
-		newPtr->data = node->value;		//my task data assingning...
-		newPtr->nextPtr = NULL;			//nextPtr is not certain yet.
+		newPtr->value = node->value;		//my task data assingning...
+//		newPtr->task_num = node->task_num;
+//		newPtr->task_type = node->task_type;
+		newPtr->next = NULL;			//nextPtr is not certain yet.
 
-		if(isEmpty( *headPtr)){			//control whether first node is exist
+		if(*headPtr == NULL){			//control whether first node is exist
 			*headPtr = newPtr;			//this is first, right
 		}
 		else {
-			(*tailPtr)->nextPtr = newPtr;		//this is not first, so connect to tail
+			(*tailPtr)->next = newPtr;		//this is not first, so connect to tail
 		}
 
 		*tailPtr = newPtr;	//our node should be tail
 	}
 	else {
-		printf("%c not inserted.No memory available.\n", value );	//malloc is unsuccessful
+		printf("%c not inserted.No memory available.\n", node->value );	//malloc is unsuccessful
 	}
 
 }
 
 void Task_queue(int n){
+	int i=0;
 
-	//n terminalden girilen sayı
-
-	tsk_node_s tsk_node_s *headPtr = NULL;
-	tsk_node_s tsk_node_s *tailPtr = NULL;
-
-	for(int i=1;i<=n;--i){
-
-		struct tsk_node_s* node = NULL;
-		node = malloc(sizeof(struct tsk_node_s));
-		node->task_num = i;
-		node->task_type = rand() % 3 + 1;
-		node->value = rand() % 20 + 1;
-		Task_enqueue(&head,&tail,&node);
-		free(node);
+	for(i=1;i<=n;i++){
+		Tsk_node_s nodetoqueue;
+		nodetoqueue.task_num = i;
+		nodetoqueue.task_type = rand() % 3 + 1;
+		nodetoqueue.value = rand() % 20 + 1;
+		Task_enqueue(&headPtr,&tailPtr,&nodetoqueue);
 	}
 
-}
+	for(i=1;i<(n+1);i++){
+		Tsk_node_s nodefromqueue;
+		Task_dequeue(&headPtr,&tailPtr,&nodefromqueue);
+		printf("%d\n",nodefromqueue.value);
 
-int Insert( lst_node_s *sPtr, int value ){
-
-	lst_node_s newPtr;
-	lst_node_s previousPtr;
-	lst_node_s currentPtr;
-
-	newPtr = malloc( sizeof( lst_node_s ) );
-
-	if ( newPtr != NULL ) {
-		newPtr->data = value;
-		newPtr->nextPtr = NULL;
-
-		previousPtr = NULL;
-		currentPtr = *sPtr;
-
-		while( currentPtr != NULL && value > currentPtr->data ) {
-			previousPtr = currentPtr;
-			currentPtr = currentPtr->nextPtr;
-		}
-
-		if ( previousPtr == NULL ) {
-			newPtr->nextPtr = *sPtr;
-			*sPtr = newPtr;
-		}
-		else {
-			previousPtr->nextPtr = newPtr;
-			newPtr->nextPtr = currentPtr;
-		}
 	}
-	else {
-		printf("%c not inserted. No memory available.\n", value);
-		return ERR;
-	}
-
-	return OK;
-}
-
-int Delete( lst_node_s *sPtr, int value ){
-
-	lst_node_s previousPtr;
-	lst_node_s currentPtr;
-	lst_node_s tempPtr;
-
-	if( value == ( *sPtr )->data ){
-		tempPtr = *sPtr;
-		*sPtr = ( *sPtr )->nextPtr;
-		free( tempPtr );
-		return value;
-	}
-	else{
-		previousPtr = *sPtr;
-		currentPtr = ( *sPtr )->nextPtr;
-
-		while ( currentPtr != NULL && currentPtr->data != value ) {
-			previousPtr = currentPtr;
-			currentPtr = currentPtr->nextPtr;
-		}
-
-		if( currentPtr != NULL ){
-			tempPtr = currentPtr;
-			previousPtr->nextPtr = currentPtr->nextPtr;
-			free( tempPtr );
-			return value;
-		}
-	}
-
-	return '\0';
 }
 
 int main(int argc, char* argv[])
 {
+	int task_count=0;
 
-printf("%d adet argüman var.\n",argc);
+	printf("%d adet argüman var.\n",argc);
 
-for(int i=0;i<argc;i++)
-	printf("%d argüman: %s\n",i,argv[i]);
+//	for(int i=0;i<argc;i++){
+//		printf("%d argüman: %s\n",i,argv[i]);
+//		task_count=atoi(argv[1]);
+//
+//	}
+	task_count=atoi(argv[1]);
+	printf("task_count = %d\n",task_count);
+	Task_queue(task_count);
 
-
-
-
-
-return 0;
+	return 0;
 }
 
 
--- queue.h
+++ queue.h
@@ -7,6 +7,9 @@ int data;
 struct lst_node_s* next;
 };
 
+typedef struct lst_node_s Lst_node_s;
+typedef Lst_node_s *LstNodePtr;
+
 /* Struct for task nodes */
 struct tsk_node_s {
 int task_num; //starting from 0
@@ -16,6 +19,9 @@ struct tsk_node_s* next;
 };
 
 typedef struct tsk_node_s Tsk_node_s;
+typedef Tsk_node_s *TskNodePtr;
+
+
 
 /* List operations */
 int Insert(int value);
@@ -23,6 +29,6 @@ int Search(int value);
 int Delete(int value);
 
 /* Task queue functions */
-//void Task_queue(int n); //generate n random tasks for the task queue
-//void Task_enqueue(Tsk_node_s **headPtr, Tsk_node_s **tailPtr, Tsk_node_s **node); //insert a new task into task queue
-//int Task_dequeue(int* task_num_p, int* task_type_p, int* value_p); //take a task from task queue
+void Task_queue(int n); //generate n random tasks for the task queue
+void Task_enqueue(TskNodePtr *headPtr, TskNodePtr *tailPtr, Tsk_node_s *node); //insert a new task into task queue
+int Task_dequeue(TskNodePtr *headPtr, TskNodePtr *tailPtr, Tsk_node_s *node); //take a task from task queue
